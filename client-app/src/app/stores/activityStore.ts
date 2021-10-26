242014f6d784ac8d0e0368f875894ce8bd1e6b58
Clayton Ribeiro <clayton.ribeiro@5by5.com.br>
2021-10-26 19:31:41 -0300

    adicionando configurações do eslint e prettier

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,136 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+
+import agent from '../api/agent';
+import { Activity } from '../models/activity';
+
+class ActivityStore {
+  activityRegistry = new Map<string, Activity>();
+  selectedActivity: Activity | undefined = undefined;
+  editMode = false;
+  loading = false;
+  loadinginitial = true;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  get activitiesByDate() {
+    return Array.from(this.activityRegistry.values()).sort(
+      (a, b) => Date.parse(a.date.toString()) - Date.parse(b.date.toString())
+    );
+  }
+
+  get groupedActivities() {
+    return Object.entries(
+      this.activitiesByDate.reduce((activities, activity) => {
+        const { date } = activity;
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: Activity[] })
+    );
+  }
+
+  loadActivities = async () => {
+    this.loadinginitial = true;
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((element) => {
+        this.setActivity(element);
+      });
+      this.setLoadingInitial(false);
+    } catch (error) {
+      console.log(error);
+      this.setLoadingInitial(false);
+    }
+  };
+
+  loadActivity = async (id: string) => {
+    let activity = this.getActivity(id);
+    if (activity) {
+      this.selectedActivity = activity;
+      return activity;
+    }
+    this.loadinginitial = true;
+    try {
+      activity = await agent.Activities.details(id);
+      this.setActivity(activity);
+      runInAction(() => {
+        this.selectedActivity = activity;
+      });
+      this.setLoadingInitial(false);
+      return activity;
+    } catch (error) {
+      console.log(error);
+      this.setLoadingInitial(false);
+    }
+  };
+
+  private setActivity = (activity: Activity) => {
+    activity.date = activity.date.split('T')[0];
+    this.activityRegistry.set(activity.id, activity);
+  };
+
+  private getActivity = (id: string) => {
+    return this.activityRegistry.get(id);
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadinginitial = state;
+  };
+
+  createActivity = async (activity: Activity) => {
+    this.loading = true;
+    try {
+      await agent.Activities.create(activity);
+      runInAction(() => {
+        this.activityRegistry.set(activity.id, activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  };
+
+  updateActivity = async (activity: Activity) => {
+    this.loading = true;
+    try {
+      await agent.Activities.update(activity);
+      runInAction(() => {
+        this.activityRegistry.set(activity.id, activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  };
+
+  deleteActivity = async (id: string) => {
+    this.loading = true;
+    try {
+      await agent.Activities.delete(id);
+      runInAction(() => {
+        this.activityRegistry.delete(id);
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  };
+}
+
+export default ActivityStore;
